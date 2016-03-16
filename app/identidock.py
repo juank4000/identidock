461514988a69e5e1a5555448443b44860e0a82a7
Juan Carlos Reyes Fernández <juank4000@gmail.com>
2016-03-16 15:09:42 -0300

    abc

--- app/identidock.py
+++ app/identidock.py
@@ -35,7 +35,7 @@ def mainpage():
 @app.route('/monster/<name>')
 def get_identicon(name):
 
-    name = html.escape(name, quote=True)
+    name = name
     image = cache.get(name)
     if image is None:
         print ("Cache miss", flush=True)
